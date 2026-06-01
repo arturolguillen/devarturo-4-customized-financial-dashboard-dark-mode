47795507c56db160bb7f633f909e93b169588033
Arturo Guillén <aguillen.js@gmail.com>
2026-06-01 18:07:07 -0400

    Fix height class in SideNavbar component for responsive design

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -2,7 +2,7 @@ export default function SideNavbar({ children }: {
     children: React.ReactNode;
 }) {
     return (
-        <aside className="bg-zinc-950 h-screen w-64 flex-col hidden lg:flex sticky top-0 left-0 border-r border-zinc-800 py-6 px-4">
+        <aside className="bg-zinc-950 h-dvh w-64 flex-col hidden lg:flex sticky top-0 left-0 border-r border-zinc-800 py-6 px-4">
             {children}
         </aside>
     );
